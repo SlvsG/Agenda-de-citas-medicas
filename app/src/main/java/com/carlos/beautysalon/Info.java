ba33dc2b5fa292422a27e8e882b58fd3cf70a8c9
analyticlimitacion <angelselvas08@hotmail.com>
2023-06-07 15:21:23 -0600

    primer commit

---
+++ app/src/main/java/com/carlos/beautysalon/Info.java
@@ -0,0 +1,20 @@
+package com.carlos.beautysalon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Info extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+
+    }
+}
